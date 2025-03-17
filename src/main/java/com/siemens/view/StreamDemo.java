167e8270da1afe39779a71b064de9348346acf57
JyotiSathe <jyotisathe1138@gmail.com>
2025-03-17 11:10:21 +0000

    refactoring

--- src/main/java/com/siemens/view/StreamDemo.java
+++ src/main/java/com/siemens/view/StreamDemo.java
@@ -14,18 +14,20 @@ import java.util.stream.Collectors;
 public class StreamDemo {
 
     public static void main(String[] args) {
-        List<Individual> individuals = ComparatorDemo.generateIndividuals();
+        List<Individual> individuals = ComparatorDemo.generateIndividuals().subList(0,20);
 
         individuals.stream()
                 .filter(individual -> individual.getDateOfBirth().getYear() < 2000)
                 .map(Individual::getFullName)
                 .forEach(System.out::println);
 
+        System.out.println();
         //count
         System.out.println("Number of people born in/before 1994: " + individuals.stream()
                 .filter(individual -> individual.getDateOfBirth().getYear() <= 1994)
                 .count());
 
+        System.out.println();
         // sorting
         individuals.stream()
                 .filter(individual -> individual.getDateOfBirth().getYear() < 2000)
@@ -34,6 +36,7 @@ public class StreamDemo {
 //                .map(individual -> String.format("Name: %s, DateOfBirth: %s", individual.getFullName(), individual.getDateOfBirth()))
                 .forEach(System.out::println);
 
+        System.out.println();
         Map<FullName, LocalDate> map = individuals.stream()
                 .filter(individual -> individual.getDateOfBirth().getYear() < 2000)
                 .collect(Collectors.toMap(Individual::getFullName, Individual::getDateOfBirth));
@@ -42,11 +45,13 @@ public class StreamDemo {
 
         // count how many male, female and others
         // grouping
+        System.out.println();
         Map<Gender, Long> groupingMap = individuals.stream()
                 .collect(Collectors.groupingBy(Individual::getGender, Collectors.counting()));
 
         groupingMap.forEach((key, value) -> System.out.println(key + " " + value));
 
+        System.out.println();
         // allMatch
         System.out.println(individuals.stream().allMatch(i -> i.getFullName().getFirstName().startsWith("A")));
 
