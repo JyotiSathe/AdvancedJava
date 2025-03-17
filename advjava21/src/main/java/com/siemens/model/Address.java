48e1c88da0f23b57139168a577da8761b80e2922
JyotiSathe <35442393+JyotiSathe@users.noreply.github.com>
2025-03-17 11:18:47 +0530

    initial commit

---
+++ advjava21/src/main/java/com/siemens/model/Address.java
@@ -0,0 +1,11 @@
+package com.siemens.model;
+
+public class Address {
+    private long addressNumber;
+    private String doorNumber;
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+    private String country;
+}
---
+++ advjava21/src/main/java/com/siemens/model/Customer.java
@@ -0,0 +1,22 @@
+package com.siemens.model;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+
+@SuperBuilder
+@EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+
+    protected long accountNumber;
+    protected FullName fullName;
+    protected List<Address> addresses;
+    protected long contactNumber;
+    protected String email;
+    protected String password;
+}
