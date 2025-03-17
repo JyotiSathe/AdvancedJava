564f05cbe560bd5500a07d35dfaeeeaf8141d1d2
JyotiSathe <jyotisathe1138@gmail.com>
2025-03-17 09:48:11 +0000

    stream demo : count

--- src/main/java/com/siemens/view/StreamDemo.java
+++ src/main/java/com/siemens/view/StreamDemo.java
@@ -13,5 +13,10 @@ public class StreamDemo {
                 .filter(individual -> individual.getDateOfBirth().getYear() < 2000)
                 .map(Individual::getFullName)
                 .forEach(System.out::println);
+
+        //count
+        System.out.println("Number of people born in/before 1994: " + individuals.stream()
+                .filter(individual -> individual.getDateOfBirth().getYear() <= 1994)
+                .count());
     }
 }
