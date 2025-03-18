5156566397a19a4ac8c67442703e3302fe4ddf73
JyotiSathe <jyotisathe1138@gmail.com>
2025-03-18 05:25:47 +0000

    safe varargs demo

---
+++ src/main/java/com/siemens/view/java9to21/SafeVarArgsDemo.java
@@ -0,0 +1,32 @@
+package com.siemens.view.java9to21;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SafeVarArgsDemo {
+    public static void main(String[] args) {
+        showSkillSets("C#");
+        showSkillSets("Java", "C#");
+        showSkillSets("Java", "C#", "CPP");
+
+//        foo(List.of("Java", "C#", "CPP"));
+    }
+
+    public static <T> void foo(List<T>... bar) {
+        // First, strip away the array type (arrays allow this kind of upcasting)
+        Object[] objectArray = bar;
+
+        // Next, insert an element with an incorrect type into the array
+        objectArray[0] = Arrays.asList(Integer.valueOf(42));
+
+        // Finally, try accessing the original array. A runtime error will occur
+        // (ClassCastException due to a casting from Integer to String)
+        T firstElement = bar[0].get(0);
+
+        System.out.println(firstElement);
+    }
+
+    private static <T> void showSkillSets(T... skills) {
+        Arrays.stream(skills).toList().forEach(System.out::println);
+    }
+}
