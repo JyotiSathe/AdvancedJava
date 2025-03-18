cbb34195a6c34bc7d6df86e46c75ab8b33303b71
JyotiSathe <jyotisathe1138@gmail.com>
2025-03-18 05:14:29 +0000

    underscore not allowed java 9 onwards

---
+++ src/main/java/com/siemens/view/java9to21/UseOfUnderscore.java
@@ -0,0 +1,8 @@
+package com.siemens.view.java9to21;
+
+public class UseOfUnderscore {
+    public static void main(String[] args) {
+        // int _ = 10; compile time error
+        int __ = 100;
+    }
+}
