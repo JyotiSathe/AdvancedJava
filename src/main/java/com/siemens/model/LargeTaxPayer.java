6a01202890c046e8a25681a99c945df719c59aa8
JyotiSathe <jyotisathe1138@gmail.com>
2025-03-18 11:32:06 +0000

    sealed - java 17

---
+++ src/main/java/com/siemens/model/LargeTaxPayer.java
@@ -0,0 +1,18 @@
+package com.siemens.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public non-sealed class LargeTaxPayer extends Individual {
+    private int extension;
+}
+
+
+
+
